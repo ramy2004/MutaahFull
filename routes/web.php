368c549376f90cf70344c465b2c329135d95a520
RamyAlsmak <47426289+ramy2004@users.noreply.github.com>
2026-08-27 15:48:50 +0300

    first commit2

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,16 @@ Route::get('/', function () {
     return response()->file(public_path('index.html'));
 });
 
+Route::get('/products/{id}', function (string $id) {
+    $staticPage = public_path('products/' . $id . '/index.html');
+
+    if (!is_file($staticPage)) {
+        $staticPage = public_path('products/1/index.html');
+    }
+
+    return response()->file($staticPage);
+})->where('id', '[^/]+');
+
 Route::get('/{path}', function (string $path) {
     $staticPage = public_path(trim($path, '/') . '/index.html');
 
